8d3cbfa707a709ab842d07ad812b6d44d192bb11
AdityaChikkerur <166357008+AdityaChikkerur@users.noreply.github.com>
2026-01-30 15:47:16 +0530

    Implement brute force method for max subarray sum
    Added brute force approach to find maximum subarray sum.

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -8,3 +8,19 @@ class Solution:
             if curSum < 0:
                 curSum=0
         return maxSum
+
+
+# BRUTE FORCE APPROACH
+'''
+arr=[5,4,-1,7,8]
+#maxSum=-999
+maxSum=float('-inf')
+for i in range(0,len(arr)):
+    curSum=0
+    for j in range(i,len(arr)):
+        curSum=curSum+arr[j]
+        if curSum> maxSum:
+            maxSum=curSum
+print(maxSum)
+'''
+    

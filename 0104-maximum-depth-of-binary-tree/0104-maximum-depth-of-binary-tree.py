2f8fdda838758f6212ad8c18f6f428ad32be03b7
AdityaChikkerur <166357008+AdityaChikkerur@users.noreply.github.com>
2026-02-17 15:59:32 +0530

    Time: 0 ms (100%), Space: 20.4 MB (43.17%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,30 +6,36 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        res=[]
-        level=[]
-        c=0
-        q=deque()
-        q.append(root)
-        q.append(None)
+        # res=[]
+        # level=[]
+        # c=0
+        # q=deque()
+        # q.append(root)
+        # q.append(None)
 
-        if root is None:
-            return c
+        # if root is None:
+        #     return 0
        
-        while q:
-            curr=q.popleft()
-            if curr is None:
-                res.append(level)
-                level=[]
-                c+=1
-                if q:
-                    q.append(None)
-                else:
-                    break
-            else:
-                level.append(curr.val)
-                if curr.left:
-                    q.append(curr.left)
-                if curr.right:
-                    q.append(curr.right)
-        return c
+        # while q:
+        #     curr=q.popleft()
+        #     if curr is None:
+        #         res.append(level)
+        #         level=[]
+        #         c+=1
+        #         if q:
+        #             q.append(None)
+        #         else:
+        #             break
+        #     else:
+        #         level.append(curr.val)
+        #         if curr.left:
+        #             q.append(curr.left)
+        #         if curr.right:
+        #             q.append(curr.right)
+        # return c
+        if root is None:
+            return 0
+        left_height=self.maxDepth(root.left)
+        right_height=self.maxDepth(root.right)
+
+        return max(left_height,right_height)+1
